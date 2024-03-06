6d6811e3767aa32b832484445dc497764a1bccfe
nurturegrit <sumitjaidka786@gmail.com>
2024-03-06 17:09:50 +0530

    Completed Mail merge exercise

--- Intermediate/Mail Merge Project Start/main.py
+++ Intermediate/Mail Merge Project Start/main.py
@@ -2,7 +2,28 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+def mail():
+    with open("./Input/Letters/starting_letter.txt",'r') as file:
+        contents = file.read()
+    return contents
+
+def name_list():
+    name_list = []
+    with open("./Input/Names/invited_names.txt",'r') as file:
+        for line in file.readlines():
+            name_list.append(line.strip())
+        return name_list
+
+def personlize_letter(name,letter):
+    letter = letter.replace('[name]',name)
+    return letter
+
+def main():
+    for name in name_list():
+        letter = mail()
+        letter = personlize_letter(name,letter)
+        with open(f'./Output/ReadyToSend/{name}.txt','w+') as file:
+            file.write(letter)
+
+if __name__ == '__main__':
+    main()
