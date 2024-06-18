3cf24a2da806ea2aac20564f844c072753322971
nurturegrit <sumitjaidka786@gmail.com>
2024-06-18 18:57:28 +0530

    Updating Nato Aphabet App

--- Intermediate/Nato_Alphabet/main.py
+++ Intermediate/Nato_Alphabet/main.py
@@ -4,7 +4,14 @@ nato_alphabet = pd.read_csv("nato_phonetic_alphabet.csv")
 
 dict_nato = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
-word = input().upper()
-print([dict_nato[char] for char in word])
+try:
+    word = input().upper()
+    for char in word:
+        if not char.isalpha():
+             raise ValueError
+except ValueError:
+    print("The Input Should Contain Alphabet Letters Only.")
+else:
+    print([dict_nato[char] for char in word])
 
 #Prints Nato Aphabet - using for spelling words on a phone call , to tell spellings of words to others
